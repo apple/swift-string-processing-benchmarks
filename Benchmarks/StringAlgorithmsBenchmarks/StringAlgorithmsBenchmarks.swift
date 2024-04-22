807c34c7c689b849b0249da6fc6da6e7af59e47a
Nate Cook <natecook@apple.com>
2024-04-22 14:19:49 -0500

    Add additional string benchmarks

--- Benchmarks/StringAlgorithmsBenchmarks/StringAlgorithmsBenchmarks.swift
+++ Benchmarks/StringAlgorithmsBenchmarks/StringAlgorithmsBenchmarks.swift
@@ -3,6 +3,11 @@ import _StringProcessing
 import TestData
 
 let benchmarks = {
+  Benchmark("firstRange/Devanagari-short") { benchmark in
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.taggedDevanagari.firstRange(of: "यब"))
+    }
+  }
   
   Benchmark("firstRange/Devanagari") { benchmark in
     for _ in benchmark.scaledIterations {
@@ -15,7 +20,15 @@ let benchmarks = {
       blackHole(TestData.swiftOrgHTML.firstRange(of: "/assets/javascripts/application.js"))
     }
   }
-  
+
+  Benchmark("firstRange/many-small") { benchmark in
+    for _ in benchmark.scaledIterations {
+      for ipAddress in TestData.ipv4Addresses {
+        blackHole(ipAddress.firstRange(of: "121"))
+      }
+    }
+  }
+
   Benchmark("ranges/Devanagari") { benchmark in
     for _ in benchmark.scaledIterations {
       blackHole(TestData.taggedDevanagari.ranges(of: "खो"))
@@ -28,18 +41,30 @@ let benchmarks = {
     }
   }
   
+  Benchmark("ranges/HTML-many") { benchmark in
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgHTML.ranges(of: "section"))
+    }
+  }
+
   Benchmark("split/Devanagari") { benchmark in
     for _ in benchmark.scaledIterations {
-      blackHole(TestData.taggedDevanagari.firstRange(of: "खो"))
+      blackHole(TestData.taggedDevanagari.split(separator: "खो" as String))
     }
   }
   
   Benchmark("split/HTML") { benchmark in
     for _ in benchmark.scaledIterations {
-      blackHole(TestData.swiftOrgHTML.firstRange(of: "/assets/javascripts"))
+      blackHole(TestData.swiftOrgHTML.split(separator: "/assets/javascripts"))
     }
   }
   
+  Benchmark("split/HTML-many") { benchmark in
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgHTML.split(separator: "section"))
+    }
+  }
+
   Benchmark("replacing/Devanagari") { benchmark in
     for _ in benchmark.scaledIterations {
       blackHole(TestData.taggedDevanagari.replacing("खो", with: "-"))
@@ -49,6 +74,16 @@ let benchmarks = {
   Benchmark("replacing/HTML") { benchmark in
     for _ in benchmark.scaledIterations {
       blackHole(TestData.swiftOrgHTML.replacing("/assets/javascripts", with: "-"))
+    }
+  }
+  Benchmark("replacing/HTML-many") { benchmark in
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgHTML.replacing("section", with: "-"))
+    }
+  }
+  Benchmark("replacing/HTML-many-small") { benchmark in
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgHTML.replacing("sec", with: "-"))
     }
   }
 }
