d9ba4cb8c378f789d189edf768e01ff38a9dc51f
Nate Cook <natecook@apple.com>
2024-05-22 19:42:55 -0500

    Move over a bunch of regex benchmarks

--- Benchmarks/RegexBenchmarks/RegexBenchmarks.swift
+++ Benchmarks/RegexBenchmarks/RegexBenchmarks.swift
@@ -40,4 +40,93 @@ let benchmarks = {
       blackHole(input.wholeMatch(of: #/.*?;/#))
     }
   }
+  
+  Benchmark("BasicBuiltinCharacterClass") { benchmark in
+    let re = #/\d\w/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+  
+  Benchmark("EmailBuiltinCharacterClass") { benchmark in
+    let re = #/(?:\d|\w|\.|-|_|%|\+)+@(?:\d|\w|\.|-|_|%|\+)+/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.validEmails.matches(of: re))
+    }
+  }
+  
+  Benchmark("BuiltIn/Words") { benchmark in
+    let re = #/\w+/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgHTML.matches(of: re))
+    }
+  }
+  Benchmark("BuiltIn/Numbers") { benchmark in
+    let re = #/\d+/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgHTML.matches(of: re))
+    }
+  }
+  Benchmark("BuiltIn/Lines") { benchmark in
+    let re = #/.+/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgHTML.matches(of: re))
+    }
+  }
+  
+  Benchmark("CSS") { benchmark in
+    let re = #/--([a-zA-Z0-9_-]+)\s*:\s*(.*?);/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.swiftOrgCSS.matches(of: re))
+    }
+  }
+  
+  Benchmark("CCC/Basic") { benchmark in
+    let re = #/[abCDeiou'~]{4,6}/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+  Benchmark("CCC/BasicRange") { benchmark in
+    let re = #/[a-z]{4,6}/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+
+  Benchmark("CCC/CaseInsensitive") { benchmark in
+    let re = #/(?i)[abCDeiou'~]{4,6}/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+
+  Benchmark("CCC/Inverted") { benchmark in
+    let re = #/[^jskldfjoi]{4,6}/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+
+  Benchmark("CCC/Subtraction") { benchmark in
+    let re = #/[a-z--[ae]]{4,6}/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+
+  Benchmark("CCC/Intersection") { benchmark in
+    let re = #/[a-z&&[abcdeiou]]{4,6}/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+
+  Benchmark("CCC/SymmetricDifference") { benchmark in
+    let re = #/[a-z~~[jskldfjoi]]{4,6}/#
+    for _ in benchmark.scaledIterations {
+      blackHole(TestData.graphemeBreakData.matches(of: re))
+    }
+  }
+
 }
